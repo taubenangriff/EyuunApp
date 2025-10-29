1ac8615a129b99f571ad4881a7bb2dda6afe7d70
taubenangriff <51975164+taubenangriff@users.noreply.github.com>
2025-10-29 18:44:17 +0100

    add shortcuts for entities

---
+++ lib/components/EntityExtensions.dart
@@ -0,0 +1,39 @@
+import 'package:flexbackend/components/standard.dart';
+import 'package:flexbackend/components/text.dart';
+import 'package:oxygen/oxygen.dart';
+
+extension EntityIdExtension on Entity {
+
+  String getTextKey() {
+    if(has<TextComponent>()){
+      var textComponent = get<TextComponent>();
+      if(textComponent!.textOverride != null){
+        return textComponent.textOverride!;
+      }
+    }
+    return getTypeId();
+  }
+
+  String? getFluff() {
+    if(has<TextComponent>()){
+      return get<TextComponent>()?.fluff;
+    }
+    return null;
+  }
+
+  String getObjectId() {
+    if(!this.has<StandardComponent>()){
+      throw Error();
+    }
+
+    return this.get<StandardComponent>()!.objectId;
+  }
+
+  String getTypeId() {
+    if(!this.has<StandardComponent>()){
+      throw Error();
+    }
+
+    return this.get<StandardComponent>()!.typeId;
+  }
+}
