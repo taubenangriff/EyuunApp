ec21b33da63d02371c2ee73d37dd459eea337777
taubenangriff <51975164+taubenangriff@users.noreply.github.com>
2025-12-21 14:31:23 +0100

    Add craftMethod.dart

---
+++ lib/components/CraftMethod.dart
@@ -0,0 +1,53 @@
+import 'package:dart_mappable/dart_mappable.dart';
+import 'package:flexbackend/core/assetLink.dart';
+
+import '../core/components/EyuunComponent.dart';
+
+part 'CraftMethod.mapper.dart';
+
+@MappableClass()
+class CraftMethodStatic with CraftMethodStaticMappable {
+  String appliedEffect;
+  int increaseSuccessThreshold;
+
+  CraftMethodStatic(this.appliedEffect, this.increaseSuccessThreshold);
+}
+
+class CraftMethodComponent extends EyuunComponent<int> {
+  static const String propertyName = "craftMethod";
+
+  late AssetLink? appliedEffect;
+  int increaseSuccessThreshold = 0;
+
+  /// Whether this craftMethod applies an effect on construction.
+  bool appliesEffect() => appliedEffect?.isValidLink() ?? false;
+
+  @override
+  String getName() => propertyName;
+
+  @override
+  void init([int? data]) {
+    reset();
+  }
+
+  @override
+  void loadDynamicData(Map<String, dynamic> dynamicData) {
+    // nothing to load here.
+  }
+
+  @override
+  void loadStaticData(Map<String, dynamic> staticData) {
+    var stat = CraftMethodStaticMapper.fromMap(staticData);
+    appliedEffect = AssetLink(stat.appliedEffect);
+    increaseSuccessThreshold = stat.increaseSuccessThreshold;
+  }
+
+  @override
+  void reset() {
+    increaseSuccessThreshold = 0;
+    appliedEffect = null;
+  }
+
+  @override
+  Map<String, dynamic> saveDynamicData() => <String, dynamic>{};
+}
--- lib/core/assetLink.dart
+++ lib/core/assetLink.dart
@@ -1,5 +1,7 @@
 import 'package:dart_mappable/dart_mappable.dart';
 import 'package:flexbackend/core/components/EntityExtensions.dart';
+import 'package:flexbackend/core/registerServices.dart';
+import 'package:flexbackend/core/services/assetloader.dart';
 import 'package:oxygen/oxygen.dart';
 
 part 'assetLink.mapper.dart';
@@ -13,4 +15,6 @@ class AssetLink with AssetLinkMappable {
   Entity getEntity() => throw UnimplementedError();
 
   static AssetLink fromEntity(Entity e) => AssetLink(e.getTypeId());
+
+  bool isValidLink() => locator<AssetLoader>().getStatic(id) != null;
 }
