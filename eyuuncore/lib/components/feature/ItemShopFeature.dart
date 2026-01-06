d8446f61cf7c709881a06634974362d381334407
taubenangriff <51975164+taubenangriff@users.noreply.github.com>
2026-01-06 20:59:28 +0100

    Add ItemShopFeature

---
+++ eyuuncore/lib/components/feature/ItemShopFeature.dart
@@ -0,0 +1,53 @@
+import 'package:eyuuncore/components/AssetBundle.dart';
+import 'package:dart_mappable/dart_mappable.dart';
+import 'package:eyuuncore/core/components/EyuunComponent.dart';
+import 'package:eyuuncore/core/reflection/Reflecting.dart';
+import 'package:eyuuncore/core/reflection/reflector.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:oxygen/oxygen.dart';
+
+import '../../core/assetLink.dart';
+part 'ItemShopFeature.mapper.dart';
+
+@MappableClass()
+@reflector
+class ItemShopFeatureStatic with ItemShopFeatureStaticMappable, ComponentReflectable {
+  AssetLink topLevelBundle;
+  ItemShopFeatureStatic(this.topLevelBundle);
+}
+
+class ItemShopFeatureComponent extends EyuunComponent<int> {
+  static const String propertyName = "itemShopFeature";
+
+  /// link to the Asset with [AssetBundleComponent] that contains all the language assets.
+  late AssetLink topLevelBundle;
+
+  List<Entity> getLanguages() => topLevelBundle.getEntity()?.get<AssetBundleComponent>()?.getAssets() ?? [];
+
+  @override
+  String getName() => propertyName;
+
+  @override
+  void init([int? data]) {
+    reset();
+  }
+
+  @override
+  void loadDynamicData(Map<String, dynamic> dynamicData) {
+    // nothing to load here
+  }
+
+  @override
+  void loadStaticData(Map<String, dynamic> staticData) {
+    var stat = ItemShopFeatureStaticMapper.fromMap(staticData);
+    topLevelBundle = stat.topLevelBundle;
+  }
+
+  @override
+  void reset() {
+    topLevelBundle = AssetLink.invalid();
+  }
+
+  @override
+  Map<String, dynamic> saveDynamicData() => <String, dynamic>{};
+}
