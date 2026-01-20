9240f35865a5618870845988e7bdcaa10f02d56c
taubenangriff <51975164+taubenangriff@users.noreply.github.com>
2026-01-20 17:40:44 +0100

    diagonal mirror matrix could be helpful on other corners too

--- lib/view/decoration/linePainters/LinePainter.dart
+++ lib/view/decoration/linePainters/LinePainter.dart
@@ -1,11 +1,13 @@
 import 'dart:ui';
 
+import 'package:eyuunapp/view/decoration/ArtDecoBoxDecoration.dart';
 import 'package:eyuunapp/view/decoration/linePainters/AbstractLinePainter.dart';
 
 class LinePainter extends AbstractLinePainter {
   LinePainter(super.primaryLinePaint);
 
   @override
+  /// Paints a top line of length [size.width]. The vertical size respected by [ArtDecoBoxDecoration] is [size.height] towards both inner and outer.
   void paint(Canvas canvas, Size size) {
     var linePaint = Paint.from(primaryLinePaint)..style = PaintingStyle.stroke;
     canvas.drawLine(const Offset(0, 0), Offset(size.width, 0), linePaint);
