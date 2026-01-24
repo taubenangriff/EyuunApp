c0cd48c0c848f13778cf3b91ed4de4c9726c3b62
taubenangriff <51975164+taubenangriff@users.noreply.github.com>
2026-01-24 12:20:10 +0100

    SkillLearning should be updated here

--- eyuuncore/lib/system/collectActionsSystem.dart
+++ eyuuncore/lib/system/collectActionsSystem.dart
@@ -29,7 +29,7 @@ class CollectActionsSystem extends System {
       Has<ActionUserComponent>(),
       Has<CombatComponent>(),
     ]);
-    combatQuery = createQuery([
+    skillLearnerQuery = createQuery([
       Has<ActionUserComponent>(),
       Has<SkillLearnerComponent>(),
     ]);
